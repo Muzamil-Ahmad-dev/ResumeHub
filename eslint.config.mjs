8fe3ce4da59a56e25d1bfc8c01a15c932c6a14c6
Muzamil Ahmad <muzamilsakhi079@gmail.com>
2025-10-31 16:18:31 +0500

    Replace ESLint config with Next.js config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,8 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  reactStrictMode: true,
+};
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
+export default nextConfig;
